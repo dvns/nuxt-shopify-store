2d2bfca8b610df927c63d5a8f97d28597d2f8f0a
Davin Suen <y6suen@gmail.com>
2021-06-09 21:23:37 -0400

    css fun

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <Nuxt />
+    <Header />
+    <main class="container mx-auto px-4">
+      <Nuxt />
+    </main>
   </div>
 </template>
 
-<style></style>
+<style lang="postcss">
+.page-title {
+  @apply font-serif tracking-widest text-base text-center uppercase my-12;
+}
+</style>
