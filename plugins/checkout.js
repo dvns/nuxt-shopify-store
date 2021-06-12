9d45f0527a679e9f80aca29f164991793e27d181
Davin Suen <y6suen@gmail.com>
2021-06-11 23:18:46 -0400

    remove console logs

--- plugins/checkout.js
+++ plugins/checkout.js
@@ -7,11 +7,11 @@ export default function ({ app, store }) {
     // Force a checkout refresh on each page load
     if (store.getters.checkoutId) {
       await store.dispatch('updateCheckout')
-      console.log('# Checkout exists. Updating checkout state.')
+      // console.log('# Checkout exists. Updating checkout state.')
 
       // If checkout is complete, create a new checkout Id
       if (store.getters.isCheckoutComplete) {
-        console.log('# Checkout is complete. Creating a new one.')
+        // console.log('# Checkout is complete. Creating a new one.')
         await store.dispatch('createCheckout')
       }
     }
