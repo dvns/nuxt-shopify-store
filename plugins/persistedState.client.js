bd9a62378d2f720be4ed69d991c9785d15b21144
Davin Suen <y6suen@gmail.com>
2021-06-12 00:28:59 -0400

    testing fix

--- plugins/persistedState.client.js
+++ plugins/persistedState.client.js
@@ -1,10 +1,8 @@
 import createPersistedState from 'vuex-persistedstate'
 
 export default ({ store }) => {
-  window.onNuxtReady(() => {
-    createPersistedState({
-      key: 'vCheckout',
-      paths: ['checkout'],
-    })(store)
-  })
+  createPersistedState({
+    key: 'vCheckout',
+    paths: ['checkout'],
+  })(store)
 }
